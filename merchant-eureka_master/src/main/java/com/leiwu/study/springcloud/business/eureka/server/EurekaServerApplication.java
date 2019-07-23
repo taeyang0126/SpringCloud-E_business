987c9be1f05d53a3fd0ea5c222d63871a511b1e7
伍磊 <17674030991@163.com>
2019-07-23 21:38:10 +0800

    [add]配置高可用（单机器三节点）eureka-server

---
+++ merchant-eureka_master/src/main/java/com/leiwu/study/springcloud/business/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.leiwu.study.springcloud.business.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka-server 服务注册中心启动类(既是server也是client)
+ *
+ * @author 伍磊
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
