ff0b4ba007ce0934afe952bc02ad1e9ffdd652ed
伍磊 <17674030991@163.com>
2019-07-23 22:17:51 +0800

    [add]将eureka客户端注册到多个节点上

---
+++ merchant-client/src/main/java/com/leiwu/study/springcloud/business/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,19 @@
+package com.leiwu.study.springcloud.business.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * eureka客户端启动类
+ *
+ * @author 伍磊
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
